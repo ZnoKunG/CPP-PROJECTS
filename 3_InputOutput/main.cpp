946df157234b240cfcb2c4c2cce0fb1c862d254e
ZnoKunG <zno.ksy@gmail.com>
2022-04-05 21:57:33 +0700

    shorten the line code

--- 3_InputOutput/main.cpp
+++ 3_InputOutput/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string> // include string variables
+// All above are Standard library components
 
 int main(){
     // Additonal function to print out data
@@ -14,8 +15,8 @@ int main(){
     std::cout << "Please type your name and age : ";
 
     //DATA INPUT
-    // direction of the arrow have to be >> because *std::cin receive the data from the terminal into our program!!!
     /* FIRST METHOD
+    // direction of the arrow have to be >> because *std::cin receive the data from the terminal into our program!!!
     std::cin >> name;
     std::cin >> age;
     */
@@ -24,14 +25,11 @@ int main(){
     std::cin >> name >> age; 
     // this function cannot get the full name with space */
 
+    // THIRD METHOD
     std::string full_name;
     std::getline(std::cin, full_name);
     std::cin >> age;
-
-
-
     
-
     std::cout << "Hello " << full_name << ". You are " << age << " years old" << std::endl;
     return 0;
 }
